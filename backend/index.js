905106e70a408eed79161dd7bf7b5a54054526ff
Patrick MacIlwaine <patrick@macilwaine.com>
2023-07-17 14:07:29 -0700

    idk again

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
 app.use(cors());
 app.use(express.json());
 
