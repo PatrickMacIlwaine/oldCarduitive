a71fc9de201a5ffaf342d7ea55a309c83aae1c6b
Patrick MacIlwaine <patrick@macilwaine.com>
2023-07-17 13:51:31 -0700

    idk

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,7 @@ app.use(express.static(path.join(__dirname, 'frontend/build')))
 app.use(cors());
 app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
-});
+
 
 let messages = {};
 let rooms = {};
@@ -159,7 +157,7 @@ app.delete('/message/:roomId', (req, res) => {
   console.log(`Room Delete -  ${req} ${res} roomId : ${roomId}`);
 });
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 3001; 
 app.listen(PORT, () => {
   console.log(`Backend server is running at http://localhost:${PORT}`);
 });
